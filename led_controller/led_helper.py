555ca243776121ec9c7a43ad04519b5f97d3e422
Max Bothe <mathebox@gmail.com>
2015-04-10 12:43:06 +0200

    Fix projection to led if angle is too width.

--- led_controller/led_helper.py
+++ led_controller/led_helper.py
@@ -4,7 +4,7 @@ def apply_brightness(brightness, r, g, b):
     return (brightness*r, brightness*g, brightness*b)
 
 def project_to_led(led_location, symbol_location, scale_factor=1):
-    pixel_angle = led_location.angle - symbol_location.angle
+    pixel_angle = led_location.angle - (symbol_location.angle % 360)
     offset_x = pixel_angle * math.pi / 180 * symbol_location.distance
     offset_y = led_location.z * symbol_location.distance / led_location.distance
     return offset_x*scale_factor, offset_y*scale_factor
