a13c0d2fa564de5bad5c248a9186ab6e33d3a332
Max Bothe <mathebox@gmail.com>
2015-04-10 11:19:32 +0200

    Imporve LEDWorldBuilder. No need to address all leds.

--- led_controller/led_world.py
+++ led_controller/led_world.py
@@ -1,15 +1,16 @@
 import math
 
 class LEDWorld(object):
-    def __init__(self, led_locations):
+    def __init__(self, led_count, led_locations):
+        self.led_count = led_count
         self.led_locations = led_locations
 
     def draw(self, detected_objects, t):
         # return array of rgb values
-        return map(
-            lambda location: self._rgb_for_led(location, detected_objects, t),
-            self.led_locations
-        )
+        rgb_values = [(0, 0, 0)]*self.led_count
+        for location in self.led_locations:
+            rgb_values[location.opc_index] = self._rgb_for_led(location, detected_objects, t)
+        return rgb_values
 
     def _rgb_for_led(self, led_location, detected_objects, t):
         # detected_objects should be sorted by distance
@@ -21,12 +22,13 @@ class LEDWorld(object):
 
 class LEDWorldBuilder(object):
     def __init__(self):
-        self.led_locations = []
+        self.led_locations = {}
 
     def build(self):
-        # maybe sort locations. think about good structur
+        # maybe sort locations. think about good structur. prevent duplicates
         # print self.led_locations
-        return LEDWorld(self.led_locations)
+        led_count = max(self.led_locations.keys()) + 1
+        return LEDWorld(led_count, self.led_locations.values())
 
     def add_led_strip(self, opc_start_index, led_count, start_x, start_y, end_x, end_y, z):
         spacing_x = (end_x - start_x) * 1.0 / led_count
@@ -35,17 +37,13 @@ class LEDWorldBuilder(object):
             current_x, current_y = start_x+i*spacing_x, start_y+i*spacing_y
             distance = math.sqrt(current_x*current_x + current_y*current_y)
             angle = math.degrees( math.acos(current_x / distance) ) - 90
-            self.led_locations.append(
-                LEDLocation(angle, distance, z, opc_start_index+i)
-            )
+            self.led_locations[opc_start_index+i] = LEDLocation(angle, distance, z, opc_start_index+i)
         return self
 
     def add_led_circle(self, opc_start_index, led_count, radius, z):
         angle_per_led = 360 / led_count
         for i in xrange(led_count):
-            self.led_locations.append(
-                LEDLocation(angle_per_led*i, radius, z, opc_start_index+i)
-            )
+            self.led_locations[opc_start_index+i] = LEDLocation(angle_per_led*i, radius, z, opc_start_index+i)
         return self
 
 class ObjectLocation(object):
