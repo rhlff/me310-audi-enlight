49fb8b1afd1392f40a2b7db3d46bb51412409bcd
Max Bothe <mathebox@gmail.com>
2015-04-23 11:49:15 +0200

    Fix led projection for led circle.

--- led_controller/led_helper.py
+++ led_controller/led_helper.py
@@ -4,7 +4,7 @@ def apply_brightness(brightness, r, g, b):
     return (brightness*r, brightness*g, brightness*b)
 
 def project_to_led(led_location, symbol_location, scale_factor=1):
-    pixel_angle = led_location.angle - symbol_location.angle
+    pixel_angle = (led_location.angle - symbol_location.angle) % 360
     pixel_angle = pixel_angle - 360 if pixel_angle > 180 else pixel_angle
     x = math.radians(pixel_angle) * symbol_location.distance
     y = led_location.z * symbol_location.distance / led_location.distance
