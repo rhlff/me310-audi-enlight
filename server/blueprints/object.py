b531df4142b70fc491b2650de612a9dea32ccea4
Jan Kossmann <kossmann.jan@gmail.com>
2015-06-01 19:59:03 -0700

    Connect lidar code with object detection

--- server/blueprints/object.py
+++ server/blueprints/object.py
@@ -7,6 +7,10 @@ from led_controller.led_objects import LEDSpot
 from server.blueprints import SettingsBlueprint
 from server.leds import get_led_controller
 
+import numpy as np
+import lidar.lidar as lidarModule
+import pickle
+
 class ObjectDetection(threading.Thread):
     def __init__(self):
         super(ObjectDetection, self).__init__()
@@ -14,7 +18,11 @@ class ObjectDetection(threading.Thread):
 
     def run(self):
         while not self.stopped:
-            pass
+            stepNumber, distance = lidar.getData()
+            if abs(minValues[stepNumber] - distance) > 50:
+                if stdValues[stepNumber] < 40:
+                    print "Detected object at angle %f with a distance of %i" % (stepNumber * 1.8, distance)
+
 
     def stop(self):
         self.stopped = True
@@ -31,6 +39,37 @@ Color_Car = (255, 226, 64)
 Color_Danger = (255, 80, 80)
 
 detection_thread = None
+
+NUMBER_OF_VALUES = 99
+NUMBER_OF_MINIMUM_VALUES_PER_AREA = 10
+
+lidar = lidarModule.Lidar()
+# graph = livePlot.LivePlot(NUMBER_OF_VALUES)
+
+values = []
+for i in range(NUMBER_OF_VALUES):
+    values.append([])
+
+stepNumber, distance = lidar.getData()
+while stepNumber <> 0:
+    stepNumber, distance = lidar.getData()
+
+print '### Step is at 0 now ###'
+
+lidar.resetDataCount()
+
+values = pickle.load(open("scan5MinimumValues", "rb" ))
+
+print '### MIN ###'
+print map(np.min, values)
+print '### MAX ###'
+print map(np.max, values)
+print '### STD ###'
+print map(np.std, values)
+print '###########'
+
+stdValues = map(np.std, values)
+minValues = map(np.min, values)
 
 def toggle_object_detection(activate):
     global detection_thread
