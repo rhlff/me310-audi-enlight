ddd52f3ba942a417ae37bfc8bf75572f55a86f0e
Jan Kossmann <kossmann.jan@gmail.com>
2015-06-06 03:47:55 +0200

    Integrate RF24 communication for Lidar. Also skip next value

--- lidar/lidar.py
+++ lidar/lidar.py
@@ -1,27 +1,44 @@
 import time
 import serial
+from RF24 import *
 
 class Lidar(object):
 
     def __init__(self):
         self.lastStep = -1
         self.dataCollected = 0
+        self.skipNext = False
 
-        self.connection = serial.Serial('/dev/ttyACM0', 115200)
-        time.sleep(2.0)
+        self.radio = RF24(RPI_BPLUS_GPIO_J8_15, RPI_BPLUS_GPIO_J8_24, BCM2835_SPI_SPEED_8MHZ)
+        pipes = [0xE8E8F0F0E1, 0xF0F0F0F0D2]
+        self.radio.begin()
+        self.radio.setRetries(15,15)
+        self.radio.openReadingPipe(1,pipes[0])
+        self.radio.setAutoAck(False)
+        self.radio.startListening()
+
+        time.sleep(1.0)
         self.avoidJunkValue()
 
     def avoidJunkValue(self):
-        self.connection.readline()
+        if self.radio.available():
+            receive_payload = self.radio.read2()
 
     def getData(self):
-        data = self.connection.readline().split(' ')
+        data = self.radio.read2().split(' ')
+        if self.skipNext:
+            self.skipNext = False
+            return -1, -1
 
         while len(data) <> 2 or data[0] == '' or int(data[0]) == self.lastStep or int(data[1]) < 0:
-            data = self.connection.readline().split(' ')
+            data = self.radio.read2().split(' ')
 
         stepNumber = int(data[0])
         distance = int(data[1])
+
+        if distance > 80000:
+            self.skipNext = True
+
 
         distance = distance if distance <= 650 else 650
 
